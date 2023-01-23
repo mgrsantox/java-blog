8572f9c0a8e95d3d1d25f6484b0d7f60939677c4
Santosh Magar <mgrsantox@gmail.com>
2023-01-23 13:51:22 +0545

    initilize

---
+++ src/test/java/com/java_blog/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_blog.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
