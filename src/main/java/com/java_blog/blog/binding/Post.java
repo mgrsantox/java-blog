d05700282a9bec2a0dfb885d1bf019689fbb3bcb
Santosh Magar <mgrsantox@gmail.com>
2023-01-24 10:14:56 +0545

    updated

--- src/main/java/com/java_blog/blog/binding/Post.java
+++ src/main/java/com/java_blog/blog/binding/Post.java
@@ -1,5 +1,6 @@
 package com.java_blog.blog.binding;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,5 +18,6 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
+    @Column(columnDefinition = "TEXT")
     private String description;
 }
